38c10f906d4ae34feb32c559ebb71d4d458899c1
yjw <79959340+yjw-it@users.noreply.github.com>
2022-03-28 18:51:07 +0900

    22-03-28 study 내용

---
+++ 로또/LottoBall.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="ball" :style="{background}">{{number}}</div>
+</template>
+
+<script>
+export default {
+    name: 'LottoBall',
+    data(){
+        let background,
+        return {
+            background,
+        }
+    }
+}
+
+</script>
+<style scoped>
+    .ball {
+        display: inline-block;
+        border: 1px solid black;
+        width: 40px;
+        height: 40px;
+        line-height: 40px;
+        font-size: 20px;
+        text-align: center;
+        margin-right: 20px;
+    }
+</style>
--- 로또/LottoGen.vue
+++ 로또/LottoGen.vue
@@ -1,12 +1,50 @@
 <template>
     <div>
-
-    </div>
+        <div>당첨 숫자</div>
+        <div id="결과창">
+            <lotto-ball v-for="ball in winBalls" :key="ball" number="5"></lotto-ball>
+        </div>
+        <div>보너스</div>
+        <lotto-ball v-if="bonus"></lotto-ball>
+        <button v-if="redo">한 번 더 !</button>
+    </div>    
 </template>
-
 <script>
+function getWinNumbers(){
+    console.log('getWinNumbers');
+    const candidate = Array(45).fill().map((v, i)=>i+1);
+    const shuffle = [];
+    while (candidate.length>0) {
+        shuffle.push(candidate.splice(Math.floor()*candidate.length), 1)[0];
+    }
+    const bonusNumber = shuffle[shuffle.length-1];
+    const getWinNumbers = shuffle.slice(0,6).sort((p,c)=>p-c);
+    return [...winBalls, bonusNumber];
+}
 
+export default ({
+    data(){
+        return {
+            winBalls:[],
+            bonus: null,
+            redo : false,
+        };
+    },
+    computed:{
+
+    },
+    methods:{
+
+    },
+    mounted() {
+
+    },
+    destroyed() {
+
+    }
+})
 </script>
+
 <style scoped>
 
 </style>
