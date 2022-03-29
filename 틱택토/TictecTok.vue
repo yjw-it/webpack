1d524dbb25cef9b7c69b0e0e76745b5ff7bf9a72
yjw <79959340+yjw-it@users.noreply.github.com>
2022-03-29 17:34:45 +0900

    0329 study

---
+++ 틱택토/TictecTok.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div>{{turn}} 님의 턴입니다.</div>
+        <table-component :table-data="tableData"></table-component>
+        <div v-if="winner">{{winner}}가 이겼습니다.</div>
+    </div>
+</template>
+
+<script>
+    import TableComponent from './TableComponent'
+    export default {
+        components:{
+            TableComponent,
+        },
+        data() {
+            return {
+                tableData :[
+                    ['', '', ''], 
+                    ['', '', ''], 
+                    ['', '', ''],
+                ],
+                turn : 'O',
+                winner : '',
+            };
+        },
+        methods: {
+            
+        },
+    };
+</script>
+
+<style scoped>
+
+</style>
