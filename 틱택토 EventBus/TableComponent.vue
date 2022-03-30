f049f3bb74d541ac2ccf68f65bceb6cd864b5619
yjw <79959340+yjw-it@users.noreply.github.com>
2022-03-30 15:09:22 +0900

    commit

--- 틱택토 EventBus/TableComponent.vue
+++ 틱택토 EventBus/TableComponent.vue
@@ -1,31 +1,15 @@
 <template>
     <table>
-        <tr-component v-for="(rowData, index) in tableData" 
-            :row-data="rowData" :row-index="index" :key="index"></tr-component>
+        <slot />
     </table>
 </template>
 
 <script>
-import TrComponent from './TrComponent'
-
 export default {
-    props:{
-        tableData : Array,
-    },
-    components:{
-        TrComponent,
-    }
+    
 }
 </script>
 
 <style>
-    table {
-        border-collapse: collapse;
-    }
-    td {
-        border: 1px solid black;
-        width: 40px;
-        height: 40px;
-        text-align: center;
-    }
+    
 </style>
